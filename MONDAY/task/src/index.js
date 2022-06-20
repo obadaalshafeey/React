2f81823678453c015eedee92cf020a179c8c576f
17-obadaalshafeey <62554357+obadaalshafeey@users.noreply.github.com>
2022-06-20 20:39:35 +0300

    2

--- MONDAY/task/src/index.js
+++ MONDAY/task/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SecondTask from './SecondTask';
+import FirstTask from './FirstTask';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SecondTask />
+    <FirstTask />
   </React.StrictMode>
 );
 
